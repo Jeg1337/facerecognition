26da1b9ef2d9699e487318242021e81a7309f05f
jeg1337 <chromealmas@gmail.com>
2019-07-28 02:20:40 +0300

    123

--- val/s1/flip.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy
-
-for i in range(101, 130):
-    image = cv2.imread('file_{}.jpg'.format(i))
-    flip_image = cv2.flip(image,1)
-    cv2.imwrite('fil_{}.jpg'.format(i), flip_image)
